1617e7eacf4d5bad53742d5ec4591d3361ae1083
Volatiana Estelle <estelleherimanana4@gmail.com>
2026-06-15 17:58:32 +0300

    k

---
+++ eggland/src/main/java/com/app/eggland/EgglandApplication.java
@@ -0,0 +1,13 @@
+package com.app.eggland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgglandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgglandApplication.class, args);
+	}
+
+}
