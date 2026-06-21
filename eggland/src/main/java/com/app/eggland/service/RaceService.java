1ca8bfceb149e34d7a6312d5a4c66949c945f150
H4rena <h4renarakotoarison@gmail.com>
2026-06-21 22:42:49 +0300

    races
    suite

--- eggland/src/main/java/com/app/eggland/service/RaceService.java
+++ eggland/src/main/java/com/app/eggland/service/RaceService.java
@@ -1,31 +1,32 @@
 package com.app.eggland.service;
 
-import org.springframework.stereotype.Service;
-
-import com.app.eggland.model.Race;
-import com.app.eggland.repository.RaceRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.eggland.model.Race;
+import com.app.eggland.repository.RaceRepository;
 
 @Service
 public class RaceService {
     @Autowired
     private RaceRepository raceRepository;
 
-    public List<Race> findAll(){
+    public List<Race> findAll() {
         return raceRepository.findAll();
     }
-    public Optional<Race> findById(Integer id) { 
-        return raceRepository.findById(id); 
+
+    public Optional<Race> findById(Integer id) {
+        return raceRepository.findById(id);
     }
-    public Race save(Race race) { 
-        return raceRepository.save(race); 
+
+    public Race save(Race race) {
+        return raceRepository.save(race);
     }
-    public void deleteById(Integer id) { 
-        raceRepository.deleteById(id); 
+
+    public void deleteById(Integer id) {
+        raceRepository.deleteById(id);
     }
-    
 }
