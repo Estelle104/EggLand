c64d9cf8c130334a8335cbe96ddb9f8c7e7e99f5
ArtekII <favrevinod64@gmail.com>
2026-06-24 21:42:38 +0300

    fix

--- eggland/src/main/java/com/app/eggland/service/LotService.java
+++ eggland/src/main/java/com/app/eggland/service/LotService.java
@@ -1,25 +1,25 @@
 // A decommenter pour tester, a commenter lors du pull request
 
-package com.app.eggland.service;
+// package com.app.eggland.service;
 
-import com.app.eggland.model.Lot;
-import com.app.eggland.repository.LotRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import com.app.eggland.model.Lot;
+// import com.app.eggland.repository.LotRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import java.util.List;
+// import java.util.List;
 
-@Service
-public class LotService {
+// @Service
+// public class LotService {
 
-    @Autowired
-    private LotRepository lotRepository;
+//     @Autowired
+//     private LotRepository lotRepository;
 
-    public List<Lot> getAllLots() {
-        return lotRepository.findAll();
-    }
+//     public List<Lot> getAllLots() {
+//         return lotRepository.findAll();
+//     }
 
-    public List<Lot> getAllLotsActifs() {
-        return lotRepository.findAllByStatutCodeIgnoreCaseOrderByIdAsc("actif");
-    }
-}
+//     public List<Lot> getAllLotsActifs() {
+//         return lotRepository.findAllByStatutCodeIgnoreCaseOrderByIdAsc("actif");
+//     }
+// }
