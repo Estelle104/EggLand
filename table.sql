de275e5b36b4e73d8b0e19905fca0ed75de408e3
Volatiana Estelle <estelleherimanana4@gmail.com>
2026-06-25 09:04:27 +0300

    m

--- table.sql
+++ table.sql
@@ -249,7 +249,7 @@ INSERT INTO StatutLot (code) VALUES ('actif'), ('reforme');
 INSERT INTO TypeTraitement (code) VALUES ('vaccin'), ('maladie'), ('medicament');
 INSERT INTO StatutOeuf (code) VALUES ('vendu'), ('casse'), ('consomme');
 INSERT INTO TypeMvt (code) VALUES ('entree'), ('sortie');
-INSERT INTO StatutClient (code) VALUES ('actif'), ('inactif');
+INSERT INTO StatutClient (code) VALUES ('actif'), ('inactif'),('en attente');
 INSERT INTO StatutVente (code) VALUES ('en_attente'), ('paye'), ('livre');
 INSERT INTO ProduitVente (code) VALUES ('oeuf'), ('poule'), ('fumier');
 INSERT INTO StatutLivraison (code) VALUES ('en_attente'), ('en_cours'), ('livre');
