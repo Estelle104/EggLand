0e9b5f862760c4e486bbf611a0ac34c42e2934d6
Toavina <sixlovatiana@gmail.com>
2026-06-28 15:08:31 +0300

    feat:recette-backend

--- eggland/src/main/java/com/app/eggland/model/MvtArgent.java
+++ eggland/src/main/java/com/app/eggland/model/MvtArgent.java
@@ -1,11 +1,21 @@
 package com.app.eggland.model;
 
-import jakarta.persistence.*;
-import lombok.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import com.app.eggland.model.TypeMvt;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "mvtargent")
@@ -33,4 +43,8 @@ public class MvtArgent {
 
     @Column(unique = true, length = 150)
     private String reference;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lot_id")
+    private Lot lot;
 }
