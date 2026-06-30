4b2a43971ff67d32b8ca4fcac11f0d1dbd52e595
RABEMANJATO Midera Nieferana <mideranieferana@gmail.com>
2026-06-30 22:14:57 +0300

    Dashboard/modif_stat_camember

--- eggland/src/main/java/com/app/eggland/repository/ReformeRepository.java
+++ eggland/src/main/java/com/app/eggland/repository/ReformeRepository.java
@@ -1,11 +1,16 @@
 package com.app.eggland.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.app.eggland.model.Reforme;
 
 @Repository
 public interface ReformeRepository extends JpaRepository<Reforme, Integer>{   
-      void deleteByLotId(Integer lotId); 
+      void deleteByLotId(Integer lotId);
+
+      @Query("SELECT COALESCE(SUM(r.nombre), 0) FROM Reforme r WHERE r.lot.id = :lotId")
+      Integer sumByLotId(@Param("lotId") Integer lotId);
 }
