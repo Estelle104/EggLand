5b877db1469cc4f59f3939869d993f1046c21fa4
Izaia-zax <Herinandrianinaizaia@gmail.com>
2026-07-02 08:27:12 +0000

    qc

--- eggland/src/main/java/com/app/eggland/service/LotService.java
+++ eggland/src/main/java/com/app/eggland/service/LotService.java
@@ -1,6 +1,7 @@
 
 package com.app.eggland.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.app.eggland.model.*;
